904f75e0ee46707efe934f1763979a0306b8b8d5
bibimuu <h.dai56@gmail.com>
2021-11-17 23:28:50 +0900

    callback function

--- index.js
+++ index.js
@@ -1,3 +1,36 @@
 var hasValue = true;
 var count = 2;
 var double = "sss";
+var person2 = {
+    name: "jack",
+    age: 22
+};
+var fruit = ["apple", "grape", "banana"];
+var book = ["business", 1500, false];
+var coffeeSize;
+(function (coffeeSize) {
+    coffeeSize[coffeeSize["SHORT"] = 0] = "SHORT";
+    coffeeSize[coffeeSize["TALL"] = 1] = "TALL";
+    coffeeSize[coffeeSize["GRANDE"] = 2] = "GRANDE";
+    coffeeSize[coffeeSize["VENTI"] = 3] = "VENTI";
+})(coffeeSize || (coffeeSize = {}));
+var coffee = {
+    hot: true,
+    size: coffeeSize.TALL
+};
+var unionType = 10;
+unionType = "ed";
+var unionType2 = [21, "sss"];
+var apple = "apple";
+var clothSize = "small";
+var cloth = {
+    color: "white",
+    size: clothSize
+};
+function add(num1, num2) {
+    return num1 + num2;
+}
+function sayHello() {
+    console.log("ss");
+}
+console.log(sayHello());
--- index.ts
+++ index.ts
@@ -1,3 +1,59 @@
 let hasValue: boolean = true;
 let count: number = 2;
 let double: string = `sss`;
+
+const person2= {
+  name: "jack",
+  age:22
+}
+
+const fruit = ["apple","grape","banana"];
+const book: [string,number,boolean] = ["business", 1500, false];
+
+enum coffeeSize  {
+  SHORT,
+  TALL,
+  GRANDE,
+  VENTI
+}
+
+const coffee = {
+  hot:true,
+  size:coffeeSize.TALL
+}
+
+let unionType: number | string = 10;
+unionType = "ed"
+
+let unionType2: (number | string)[] = [21,"sss"];
+
+type clothSize = "small" | "medium" | "large";
+
+const apple: "apple" = "apple";
+let clothSize : clothSize = "small";
+const cloth = {
+  color:"white",
+  size: clothSize
+}
+
+function add(num1: number,num2: number) {
+  return num1 + num2
+}
+
+function sayHello (): void {
+  console.log("ss")
+}
+
+const anotherAdd: (n1:number, n2:number) => number = function (num1: number,num2: number) {
+  return num1 + num2
+};
+
+function doubleAndHandle(num:number, cb:(num: number)=>number): void {
+  //cbは関数の名前として扱われる
+  const doubleNum = cb(num*2);
+  console.log(doubleNum);
+}
+
+doubleAndHandle(22,doubleNum =>{
+  return doubleNum
+} );
